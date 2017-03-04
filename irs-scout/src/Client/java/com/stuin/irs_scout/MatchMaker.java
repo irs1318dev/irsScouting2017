6a2f9b392cebf67a449fb7dce1f6925b867247b2
Stuart <stuin01@gmail.com>
2017-03-04 10:13:12 -0800

    match alliance corrected and boiler team tasks connected

--- irs-scout/src/Client/java/com/stuin/irs_scout/MatchMaker.java
+++ irs-scout/src/Client/java/com/stuin/irs_scout/MatchMaker.java
@@ -7,10 +7,7 @@ import com.stuin.irs_scout.Data.Match;
 import com.stuin.irs_scout.Data.Measure;
 import com.stuin.irs_scout.Views.Page;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Stuart on 2/14/2017.
@@ -20,6 +17,7 @@ class MatchMaker {
 
     private PageManager pageManager;
     private List<Page> pages;
+    private Queue<String> nextTeams;
     protected TextView status;
     protected List<Measure> data;
 
@@ -36,7 +34,7 @@ class MatchMaker {
             @Override
             public void run(List<String> s) {
                 match = new Gson().fromJson(s.get(0), Match.class);
-                if(MainActivity.position.charAt(0) != match.alliance.charAt(0)) match = new Gson().fromJson(s.get(1), Match.class);
+                if(MainActivity.position.toLowerCase().charAt(0) != match.alliance.charAt(0)) match = new Gson().fromJson(s.get(1), Match.class);
 
                 String title = "Match: " + match.match;
                 if(!MainActivity.position.contains("Fuel")) title += " Team: " + match.getTeam(MainActivity.position);
@@ -48,11 +46,17 @@ class MatchMaker {
                     public void run(List<String> measures) {
                         Gson gson = new Gson();
                         for(String s : measures) if(!s.contains("end")) data.add(gson.fromJson(s, Measure.class));
+                        if(!nextTeams.isEmpty()) new Set().start(nextTeams.poll());
                         setMatch();
                     }
                 }
                 if(!MainActivity.position.contains("Fuel")) new Set().start("/matchteamtasks?team=" + match.getTeam(MainActivity.position));
-                else setMatch();
+                else {
+                    new Set().start("/matchteamtasks?team=" + match.getTeam("1"));
+                    nextTeams = new ArrayDeque<>();
+                    nextTeams.add("/matchteamtasks?team=" + match.getTeam("2"));
+                    nextTeams.add("/matchteamtasks?team=" + match.getTeam("3"));
+                }
             }
         }
         new Data().start("/matchteams");
@@ -63,7 +67,7 @@ class MatchMaker {
         for(Page p : pages) {
             Map<String, Measure> pageData = new HashMap<>();
             for(Measure m : data) {
-                if(m.phase.equals(p.name)) pageData.put(m.task, m);
+                if(m.phase.equals(p.name)) pageData.put(m.task + ':' + m.team, m);
             }
             p.setMeasures(pageData, match);
         }
