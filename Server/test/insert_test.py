79374cb02e0f22586a7077c25d13cb76ca53e3b2
Stacy Irwin <stacy.k.irwin@gmail.com>
2017-03-03 21:31:55 -0800

    matchteamtasks works!

--- Server/test/insert_test.py
+++ Server/test/insert_test.py
@@ -8,7 +8,7 @@ from scouting.match import MatchDal
 engine = db.getdbengine()
 conn = engine.connect()
 
-def insertTestData(filename = "auto_test_data.csv"):
+def insertTestData(task, filename = "auto_test_data.csv"):
     # Change Python working directory to same as this module
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
@@ -21,6 +21,6 @@ def insertTestData(filename = "auto_test_data.csv"):
     # Insert each row into measures column
     for row in data_rdr:
         mtch = "{0:0>3}-q".format(row['match'])
-        MatchDal.matchteamtask(row['team'], 'shootLowBoiler', mtch, 'auto',
-                               attempt_count = 3,
-                               success_count=row['shootLowBoiler'])
+        MatchDal.matchteamtask(row['team'], task, mtch, 'auto',
+                               attempt_count = 3 + int(row[task]),
+                               success_count=row[task])
