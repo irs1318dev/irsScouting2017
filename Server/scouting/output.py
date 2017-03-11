0add0c98f1114257cb227015f5c9dca781759b1f
Samika Gupta <samika2000@gmail.com>
2017-03-11 15:55:14 -0800

    New file : output.py made get_sum() in output.py

--- Server/scouting/output.py
+++ Server/scouting/output.py
@@ -1,10 +1,8 @@
 import db
 from sqlalchemy.sql import text
-import pandas
+import pandas as pd
 import tkFileDialog
-import xlsxwriter
-
-
+from collections import OrderedDict
 
 def get_sum():
     engine = db.getdbengine()
@@ -14,14 +12,28 @@ def get_sum():
         "FROM ((teams FULL OUTER JOIN measures ON teams.id = measures.team_id) LEFT JOIN tasks ON tasks.id = measures.task_id) LEFT JOIN phases ON phases.id = measures.phase_id "
         " GROUP BY teams.name, tasks.name, phases.name"
         " ORDER BY teams.name, phases.name, tasks.name;")
-    result_df = pandas.read_sql(select, conn)
+    result_df = pd.read_sql(select, conn)
 
-    file_path =  tkFileDialog.asksaveasfile()
-    workbook = xlsxwriter.Workbook(file_path)
-
-    result_df.to_excel(workbook, sheet_name= 'Rankings')
-
-#get_sum("climbRope", "teleop", "success")
-#get_sum("placeGear", "auto", "attempt")
+    #file_path =  tkFileDialog.asksaveasfilename(defaultextension = 'xlsx',
+                                                #title = "Save Rankings File")
 
 
+    #result_df.to_excel(file_path, sheet_name= 'Rankings', index = 'team')
+    #del result_df['task']
+    #del result_df['sum_attempts']
+    #result_df = result_df.pivot(index='team')
+
+    team_col = result_df['team'].unique()
+    phases = result_df['phase'].unique()
+    tasks = result_df['task'].unique()
+
+    unstacked_table = OrderedDict()
+    unstacked_table['team'] = team_col
+    for phase in phases:
+        for task in tasks:
+            col_name = str(phase) + '_' + str(task)
+            unstacked_table[col_name] = pd.Series([])
+            for team in team_col:
+                val = result_df.loc[(result_df['phase'] == phase) & (result_df['task'] == task) & (result_df['team'] == team)]['sum_successes']
+                unstacked_table[col_name].append(val)
+    print unstacked_table
