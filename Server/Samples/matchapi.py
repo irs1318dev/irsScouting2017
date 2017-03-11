4d3944461d422f2c10c72cdd782d552f4bec3438
Jpowaz100 <jonahpowaz7@gmail.com>
2017-03-11 10:21:15 -0800

    Added get match scores to first api and @cherrypy.expose to matchapi.py

--- Server/Samples/matchapi.py
+++ Server/Samples/matchapi.py
@@ -6,4 +6,10 @@ class MatchResults(object):
     def result(self):
         return firstapi.getMatchResults('WAAMV', '2017', '33', 'qual')
 
+
+class MatchScores(object):
+    @cherrypy.expose
+    def result(self):
+        return firstapi.getMatchScores('WAAMV', '2017', 'qual')
+
 cherrypy.quickstart(MatchResults())
