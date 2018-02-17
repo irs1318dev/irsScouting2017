d571d917688ecfff8d6b72decd1c009381fc96b7
Stacy Irwin <stacy.k.irwin@gmail.com>
2018-02-16 18:31:10 -0800

    Added random_task function.

--- server/model/random_data.py
+++ server/model/random_data.py
@@ -1,8 +1,14 @@
 """This module contains functions for randomly generating test data.
 """
+import random
+
+import numpy.random
 import sqlalchemy
 
 import server.model.connection as sm_connection
+import server.model.dal as sm_dal
+import server.model.match as sm_match
+
 
 def create_event(rnd_event, base_event):
     """
@@ -30,4 +36,29 @@ def create_event(rnd_event, base_event):
     conn.close()
 
 
-create_event("test_holoviews", "turing")
+# create_event("test_holoviews", "turing")
+
+
+def add_poisson_measures(event, task, phase, mean_attempts, acc_min, acc_max,
+                         print_output=True, sql_output=False):
+    conn = sm_connection.engine.connect()
+    sql = sqlalchemy.text("SELECT level, match, alliance, team, station "
+                          "FROM schedules WHERE event = :evt "
+                          "and level = 'qual';")
+    results = conn.execute(sql, evt=event)
+    for row in results:
+        attempts = numpy.random.poisson(mean_attempts)
+        accuracy = numpy.random.uniform(acc_min, acc_max)
+        successes = round(attempts * accuracy)
+        if print_output:
+            print(row["team"], task, row["match"], phase, attempts, successes,
+                      accuracy)
+        if sql_output:
+            sm_match.MatchDal.insert_match_task(row["team"], task, row["match"],
+                                                phase, attempt_count=attempts,
+                                                success_count=successes)
+
+
+def test_add_measures():
+    add_poisson_measures("test_holoviews", "placeSwitch", "teleop", 7, 0.5, 1,
+                         sql_output=True)
