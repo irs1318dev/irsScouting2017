2615c00fab47e396c5021c369ebc41c9fb1053d8
stacy.k.irwin@gmail.com <stacy.k.irwin@gmail.com>
2019-01-27 13:47:06 -0800

    Removed stacy_test files.

--- server/tests/stacy_test/connection.py
+++
@@ -1,6 +0,0 @@
-import server.model.connection as smc
-
-def test_connection_string():
-    res = smc.create_conn_string()
-    print()
-    print(res)
--- server/tests/stacy_test/enum_test.py
+++
@@ -1,5 +0,0 @@
-import server.model.match as smm
-
-def test_enum():
-    smm.MatchDal.insert_match_task('5468', 'startItem', '007-q',
-                                              'auto', 'Cargo', 0, 0, 0)
