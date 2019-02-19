7eb3faa80e94c35b762db5902e299908f83a22a1
Meghana Annamaneni <meghana_annamaneni@outlook.com>
2019-02-18 16:43:21 -0800

    Fixed error in hatches_6t and changed graph title

---
+++ server/tests/season/test_sixteam.py
@@ -0,0 +1,9 @@
+import os
+
+import server.season.s2019.view.sixteam as sixteam
+
+
+def test_sixteam():
+    #sixteam.hatches_6t('010-q')
+    #sixteam.cargo_6t('010-q')
+    sixteam.pages_6t('010-q')
