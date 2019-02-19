1aeb08cae17e4bffeb7659e08fb5704fee63e750
Meghana Annamaneni <meghana_annamaneni@outlook.com>
2019-02-18 16:58:50 -0800

    Fixed the error in the hatches_sixt function and changed the graph titles.

--- server/tests/season/test_sixteam.py
+++ server/tests/season/test_sixteam.py
@@ -1,9 +1,12 @@
+import os.path
 import os
 
 import server.season.s2019.view.sixteam as sixteam
+import server.model.event as sme
 
 
 def test_sixteam():
-    #sixteam.hatches_6t('010-q')
-    #sixteam.cargo_6t('010-q')
+    sme.EventDal.set_current_event('test_data', '2019')
+    sme.EventDal.set_current_match('010-q')
+    os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), r"..\..\..\..\scouting2019")))
     sixteam.pages_6t('010-q')
