c956bd84849ae9aba80be275345302af4d8fd0a7
Akhil Kondepudi <junnu02@gmail.com>
2019-02-23 15:00:02 -0800

    added points chart, fixed ranking table, added a test for points chart in test_charts

---
+++ server/season/s2019/view/pointschart.py
@@ -0,0 +1,44 @@
+import os
+import bokeh.models as bmodels
+import bokeh.plotting as plt
+import bokeh.palettes as bpalettes
+import bokeh.models.widgets as bmw
+import bokeh.layouts as blt
+import bokeh.io
+
+import server.season.s2019.view.rankingtable as rt
+import server.model.event as sme
+import server.config as sc
+
+
+def point_chart():
+    df_temp = rt.ranking_df()
+    df_new = df_temp.filter(['avgHatchPoints', 'avgCargoPoints', 'avgClimbPoints', 'avgHabPoints'], axis=1)
+    points_cds = bmodels.ColumnDataSource(df_new)
+    task = points_cds.column_names[1:]
+    tooltips = [
+        ("", "team: @index"),
+        ("", "$name: @$name")
+    ]
+    p = plt.figure(title='Points Chart', x_range=points_cds.data['index'],
+                   plot_width=1100, plot_height=350, tooltips=tooltips, toolbar_location="above")
+    hr = p.vbar_stack(task, x='index', width=0.4,
+                      source=points_cds, color=bpalettes.RdBu4)
+    legend = bokeh.models.Legend(items=[(x, [hr[task.index(x)]]) for x in task], location=(0, 0))
+    p.add_layout(legend, 'right')
+    p.xaxis.major_label_orientation = 3.14 / 4
+    return p
+
+
+def pages_pointschart():
+    match = sme.EventDal.get_current_match()
+    os.chdir(sc.output_path('2019'))
+    chart = point_chart()
+    div = blt.WidgetBox(bmw.Div(text='<h1>Points Chart</h1>' +
+                                     'updated at match:' + match))
+    bokeh.io.output_file('pointschart.html')
+    col = blt.column([div, chart])
+    title = 'Ranking Table: Match ' + match
+    bokeh.io.save(col, title=title)
+
+
--- server/tests/season/test_charts.py
+++ server/tests/season/test_charts.py
@@ -4,6 +4,7 @@ import os
 import server.season.s2019.view.sixteam as sixteam
 import server.model.event as sme
 import server.season.s2019.view.oneteam as oneteam
+import server.season.s2019.view.pointschart as pointschart
 
 
 def test_sixteam():
@@ -18,3 +19,8 @@ def test_oneteam():
     sme.EventDal.set_current_event('test_data', '2019')
     sme.EventDal.set_current_match('030-q')
     oneteam.pages_1t('1318')
+
+def test_pointschart():
+    sme.EventDal.set_current_event('test_data', '2019')
+    sme.EventDal.set_current_match('020-q')
+    pointschart.pages_pointschart()
