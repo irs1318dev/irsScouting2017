dc92fd15c8a611861d4701bacde236e9267b02c4
stacy.k.irwin@gmail.com <stacy.k.irwin@gmail.com>
2019-02-23 16:26:29 -0800

    Revsied one-team chart and added function to create multiple one-team charts for a list of teams.

--- server/tests/season/s2019/view/test_oneteam.py
+++ server/tests/season/s2019/view/test_oneteam.py
@@ -1,12 +1,9 @@
-import server.model.connection as smc
+import server.model.event as sme
 import server.season.s2019.view.oneteam as oneteam
 
 
-def test_get_cds():
-    conn = smc.pool.getconn()
-    meas_df = oneteam.onet_d('3223', conn)
-    tasks = ['getHatch', 'csHatch', 'rocketHatch1', 'rocketHatch2',
-               'rocketHatch3']
-    print(oneteam.get_cds_averages(meas_df, tasks, 4).column_names)
-
-    smc.pool.putconn(conn)
+def test_plot():
+    sme.EventDal.set_current_event('test_data', '2019')
+    sme.EventDal.set_current_match('030-q')
+    teams = ['360', '3223']
+    oneteam.pages_1t(teams)
--- server/tests/season/test_charts.py
+++ server/tests/season/test_charts.py
@@ -13,15 +13,3 @@ def test_sixteam():
     os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__),
                                           r"..\..\..\..\scouting2019")))
     sixteam.pages_6t('010-q')
-
-
-def test_oneteam():
-    sme.EventDal.set_current_event('test_data', '2019')
-    sme.EventDal.set_current_match('030-q')
-    oneteam.pages_1t('1318')
-
-
-def test_pointschart():
-    sme.EventDal.set_current_event('test_data', '2019')
-    sme.EventDal.set_current_match('020-q')
-    pointschart.pages_pointschart()
