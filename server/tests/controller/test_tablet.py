64ba386445948b0cc93d56aafffd4e406a573995
Sravani Nanduri <sravani.nanduri@outlook.com>
2019-03-01 19:56:49 -0800

    Added header to sixteam

--- server/tests/controller/test_tablet.py
+++ server/tests/controller/test_tablet.py
@@ -2,20 +2,25 @@ import pytest
 import server.scouting.tablet as tablet
 
 
-#tests the gettablets with the initial setup
 def test_constructor():
+"""tests the gettablets with the initial setup
+"""
     tablets = tablet.TabletList()
     result = tablets.gettablets()
     assert result == 'TestSystem:Waiting    <br>   '
 
-#testing function inserttablets from tablet.py
+
 def test_insertTablets():
+"""testing function inserttablets from tablet.py
+"""
     tablets = tablet.TabletList()
     result = tablets.inserttablets("{TestSystem}")
     assert result == "Waiting"
 
-#testing that duplicate tablets aren;t added to the tablet list
+
 def test_settablet():
+"""testing that duplicate tablets aren;t added to the tablet list
+"""
     tablets = tablet.TabletList()
     newTablet = tablet.Tablet("testOne", "Auto", 1)
     result = tablets.settablet(newTablet)
@@ -28,8 +33,10 @@ def test_settablet():
     result = tablets.settablet(newTablet)
     assert tablets.length() == 2
 
-#testing that the booleans are correct for page
+
 def test_settable_assignNewPage():
+"""testing that the booleans are correct for page
+"""
     tablets = tablet.TabletList()
     newTablet = tablet.Tablet("testOne", "Auto", 1)
     result = tablets.settablet(newTablet)
@@ -41,8 +48,10 @@ def test_settable_assignNewPage():
     result = tablets.settablet(newTablet)
     assert tablets.getIndex(0).page == "Waiting"
 
-#testing that if all tablets are set to waiting, the next match can start
+
 def test_settablet_nextmatch():
+"""testing that if all tablets are set to waiting, the next match can start
+"""
     tablets = tablet.TabletList()
     newTablet = tablet.Tablet("testOne","Waiting",1)
     tablets.settablet(newTablet)
@@ -50,8 +59,10 @@ def test_settablet_nextmatch():
     tablets.settablet(newTablet)
     assert tablets.getIndex(0).page == "Reset"
 
-#testing method findnext in tablet.py
+
 def test_findnext():
+"""testing method findnext in tablet.py
+"""
     tablets = tablet.TabletList()
     newTablet = tablet.Tablet("testTwo", "Waiting","2")
     result = tablets.findnext(newTablet)
