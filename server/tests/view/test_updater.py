ed50e9814f3ffaf05fc393783ec91ef81a95efc5
Jonah Powazek <jonahpowaz7@gmail.com>
2019-03-09 13:44:52 -0800

    fixed error in test_updater that caused failure

--- server/tests/view/test_updater.py
+++ server/tests/view/test_updater.py
@@ -6,17 +6,17 @@ import os
 
 
 def test_updater():
-    u.update_graph()
     c = datetime.datetime.now().strftime('%D %H:%M')
+    u.update_graph()
     a = time.localtime(os.path.getmtime(sc.output_path('2019') + '\\rankingtable.html'))
     b = time.strftime("%D %H:%M", a)
-    assert c == b
+    assert c <= b
     a = time.localtime(os.path.getmtime(sc.output_path('2019') + '\\oneteam_index.html'))
     b = time.strftime("%D %H:%M", a)
-    assert c == b
+    assert c <= b
     a = time.localtime(os.path.getmtime(sc.output_path('2019') + '\\index.html'))
     b = time.strftime("%D %H:%M", a)
-    assert c == b
+    assert c <= b
     a = time.localtime(os.path.getmtime(sc.output_path('2019') + '\\pointschart.html'))
     b = time.strftime("%D %H:%M", a)
-    assert c == b
+    assert c <= b
