8e6365f8293a491eec25128fd5f33cab39be72a5
Jonah Powazek <jonahpowaz7@gmail.com>
2019-03-09 13:15:45 -0800

    changes to test_updater

--- server/season/s2019/view/updater.py
+++ server/season/s2019/view/updater.py
@@ -7,9 +7,34 @@ import server.season.s2019.view.pointschart as p
 
 def update_graph():
     teams = sme.EventDal.last_match_teams()
+    # Ranking Table
+    update_ranking()
+    # Points Chart
+    update_points()
+    # One Team Chart
+    update_one_team(teams)
+    # Six Team Chart
+    update_six_team()
+    # Index Pages
+    update_indexes()
+
+
+def update_ranking():
     r.pages_rankingtable()
+
+
+def update_one_team(teams):
     o.pages_1t(teams)
+
+
+def update_six_team():
     s.next3('1318')
+
+
+def update_points():
     p.pages_pointschart()
+
+
+def update_indexes():
     s.index_page()
     o.index_page1t()
--- server/tests/view/test_updater.py
+++ server/tests/view/test_updater.py
@@ -1,6 +1,22 @@
-import server.model.event as sme
 import server.season.s2019.view.updater as u
+import server.config as sc
+import time
+import datetime
+import os
 
 
 def test_updater():
     u.update_graph()
+    c = datetime.datetime.now().strftime('%D %H:%M')
+    a = time.localtime(os.path.getmtime(sc.output_path('2019') + '\\rankingtable.html'))
+    b = time.strftime("%D %H:%M", a)
+    assert c == b
+    a = time.localtime(os.path.getmtime(sc.output_path('2019') + '\\oneteam_index.html'))
+    b = time.strftime("%D %H:%M", a)
+    assert c == b
+    a = time.localtime(os.path.getmtime(sc.output_path('2019') + '\\index.html'))
+    b = time.strftime("%D %H:%M", a)
+    assert c == b
+    a = time.localtime(os.path.getmtime(sc.output_path('2019') + '\\pointschart.html'))
+    b = time.strftime("%D %H:%M", a)
+    assert c == b
